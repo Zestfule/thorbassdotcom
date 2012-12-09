e6d950286894b56abe1a45528df12d4f682cd288
Erling Thorkildsen <zestfule@gmail.com>
2012-12-08 22:10:32 -0500

    test

---
+++ config/MySQL/MySQL_Conf.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: zestfule
+ * Date: 12/8/12
+ * Time: 10:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class MySQL_Conf
+{
+
+
+
+}
